1c4699cdd988cfb20f63dac9d3b2e8c8afb3fb7a
Sumaiyya2212 <98161123+Sumaiyya2212@users.noreply.github.com>
2023-04-17 17:34:16 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,44 @@
+package bookapplication;
+
+public class Book {
+	
+	    private int bookId;
+	    private String bookName;
+	    private double bookprice;
+	    
+
+	public int getBookId() {
+			return bookId;
+		}
+
+		public void setBookId(int bookId) {
+			this.bookId = bookId;
+		}
+
+		public String getBookName() {
+			return bookName;
+		}
+
+		public void setBookName(String bookName) {
+			this.bookName = bookName;
+		}
+
+		public double getBookprice() {
+			return bookprice;
+		}
+
+		public void setBookprice(double bookprice) {
+			this.bookprice = bookprice;
+		}
+
+	public Book(int bookId, String bookName, double bookprice) {
+		super();
+		this.bookId=bookId;
+		this.bookName=bookName;
+		this.bookprice=bookprice;
+	}
+	public Book()
+	{
+		super();
+	}
+}
